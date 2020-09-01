20a62c74ef1b4655e93938929f329dfccc6577d6
Henrique Negri <henriquenegri84@gmail.com>
2020-09-01 17:40:00 -0300

    terminando pagina do mapa

---
+++ src/utils/accessCountriesMap.js
@@ -0,0 +1,24 @@
+const accessCountriesMap = [
+  "Brazil",
+  "India",
+  "US",
+  "Colombia",
+  "Peru",
+  "Argentina",
+  "Spain",
+  "Mexico",
+  "Russia",
+  "Philipines",
+  "Iraq",
+  "France",
+  "South Africa",
+  "Bangladesh",
+  "Iran",
+  "Indonesia",
+  "Ukraine",
+  "Chile",
+  "Israel",
+  "Ethiopia",
+];
+
+export default accessCountriesMap;
